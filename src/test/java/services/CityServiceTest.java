a66f92ff278c4b71190ecbd975e3ec3620d9f47d
u19215176 <vnsyrkin@sberbank.ru>
2021-05-25 17:04:04 +0500

    1. rename messege(), sortedByCapital() 2. sortedByDistrictAndName, listOfCitiesToArray remove streamAPI 3. remove unused methods in numberOfCitiesInRegion()

--- src/test/java/services/CityServiceTest.java
+++ src/test/java/services/CityServiceTest.java
@@ -19,12 +19,14 @@ public class CityServiceTest extends TestCase {
 
         actual = Arrays.asList(new City(2, "Тавда", "Свердловская область", "Урал", 40000, "1840")
                 , new City(1, "Ревда", "Свердловская область", "Аурал", 45000, "1930")
+                , new City(3, "Челябинск", "Челябинская область", "Урал", 950000, "1900")
         );
     }
 
     public void testSortByName() {
         expected = Arrays.asList(new City(1, "Ревда", "Свердловская область", "Аурал", 45000, "1930")
                 , new City(2, "Тавда", "Свердловская область", "Урал", 40000, "1840")
+                , new City(3, "Челябинск", "Челябинская область", "Урал", 950000, "1900")
         );
         Assert.assertEquals(expected, cityService.sortByName(actual));
     }
@@ -32,17 +34,18 @@ public class CityServiceTest extends TestCase {
     public void testSortByDistrictAndName() {
         expected = Arrays.asList(new City(1, "Ревда", "Свердловская область", "Аурал", 45000, "1930")
                 , new City(2, "Тавда", "Свердловская область", "Урал", 40000, "1840")
+                , new City(3, "Челябинск", "Челябинская область", "Урал", 950000, "1900")
         );
         Assert.assertEquals(expected, cityService.sortByDistrictAndName(actual));
     }
 
     public void testListOfCitiesToArray() {
-        String expectedString = "[1] = 45000";
+        String expectedString = "[3] = 950000";
         Assert.assertEquals(expectedString, cityService.listOfCitiesToArray(actual));
     }
 
     public void testNumberOfCitiesInRegion() {
-        List<String> expectedList = Arrays.asList("Свердловская область: 2");
+        List<String> expectedList = Arrays.asList("Свердловская область: 2", "Челябинская область: 1");
         Assert.assertEquals(expectedList, cityService.numberOfCitiesInRegion(actual));
     }
 }
